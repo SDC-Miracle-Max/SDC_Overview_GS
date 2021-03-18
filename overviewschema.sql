4a1e27d875f54cf56c9ff90f45377ce24da19a39
Greta Schock <gschockcodes@gmail.com>
2021-03-18 16:21:25 -0700

    Uploaded schema 1.0

--- overviewschema.sql
+++ overviewschema.sql
@@ -1,47 +1,62 @@
 psql; 
 
-CREATE DATABASE IF NOT EXISTS overview;
+CREATE DATABASE IF NOT EXISTS Overview;
 
 \c overview;
 
-CREATE TABLE IF NOT EXISTS Product_Info (
-    product_id serial,
-    product_name varchar,
-    product_slogan varchar
-    product_description varchar,
-    product_category varchar,
-    product_default_price varchar,
-    product_styles_id serial <-------this could be a number? How do we refer to foreign keys 
-)
+CREATE TABLE IF NOT EXISTS Product_List (
+    product_id INT,
+    product_name VARCHAR (100),
+    product_slogan VARCHAR (250),
+    product_description VARCHAR (250),
+    product_category VARCHAR (100),
+    product_default_price INT,
+    PRIMARY KEY (product_id)
+);
 
-CREATE TABLE IF NOT EXISTS Related_Products (
-    product_id serial,
-    related_product_id serial
-)
 
-CREATE TABLE IF NOT EXISTS Product_Features (
-    product_id serial, 
-    product_features_feature varchar, 
-    product_features_value varchar
-)
+CREATE TABLE IF NOT EXISTS Features (
+    features_id INT
+    product_id INT, 
+    product_features_feature VARCHAR (100), 
+    product_features_value VARCHAR (100),
+    PRIMARY KEY (features_id)
+);
 
-CREATE TABLE IF NOT EXISTS Product_Styles (
-    product_styles_id serial, 
-    product_styles_name varchar, 
-    product_styles_original_price varchar, 
-    product_styles_sale_price varchar, 
-    product_styles_default boolean
-)
+CREATE TABLE IF NOT EXISTS Related (
+    related_id INT, 
+    current_product_id INT, 
+    related_product_id INT,
+    PRIMARY KEY (related_id)
 
-CREATE TABLE IF NOT EXISTS Product_Styles_Photos  (
-    styles_id serial, 
-    product_styles_photos_thumbnail_url varchar, 
-    product_styles_photos_url varchar
-)
+);
 
-CREATE TABLE IF NOT EXISTS Product_Styles_SKUS (
-    styles_id serial, 
-    product_styles_SKUS_id serial, 
-    product_styles_SKUS_quantity varchar, 
-    product_styles_SKUS_size varchar
-)
+
+CREATE TABLE IF NOT EXISTS Styles (
+    styles_id INT, 
+    product_id INT, 
+    styles_name VARCHAR (100), 
+    styles_original_price INT, 
+    styles_sale_price INT, 
+    styles_default_style BOOLEAN,
+    PRIMARY KEY (styles_id)
+
+);
+
+CREATE TABLE IF NOT EXISTS SKUS (
+    skus_id INT, 
+    styles_id INT, 
+    skus_size VARCHAR (100), 
+    skus_quantity VARCHAR (100),
+    PRIMARY KEY (skus_id)
+);
+
+CREATE TABLE IF NOT EXISTS Photos (
+    photos_id INT, 
+    styles_id INT, 
+    photos_url VARCHAR (500), 
+    photos_thumbnail_url VARCHAR (500), 
+    PRIMARY KEY (photos_id)
+);
+
+
