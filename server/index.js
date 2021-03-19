cce883b91a458a41cce4fc687639b3b13b4eb80b
Greta Schock <gschockcodes@gmail.com>
2021-03-19 08:42:54 -0700

    Initial set up for db and server communication

--- server/index.js
+++ server/index.js
@@ -1,11 +1,31 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const morgan = require('morgan');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+//LIST PRODUCTS - GET /products 
+app.get('/products', (req, res) => {
+  //query to db
+  if (err) {
+    console.log(err);
+    res.send(500);
+  } else {
+    console.log('success!', data)
+  }
 })
 
+//PRODUCT INFORMATION - GET /products/:product_id
+
+//PRODUCT STYLES - GET /products/:product_id/styles 
+
+//RELATED PRODUCTS - GET /products/:product_id/related 
+
+
+
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`)
+  console.log(`SDC_Overview_GS server up and running at http://localhost:${port} :)`)
 })
