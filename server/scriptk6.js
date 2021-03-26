c6e00d60af52c87db7d74daf3fa8cdaef55e2264
Greta Schock <gschockcodes@gmail.com>
2021-03-25 20:33:02 -0700

    Reconfigured K6 testing

--- server/scriptk6.js
+++ server/scriptk6.js
@@ -1,10 +1,6 @@
 import http from 'k6/http';
 
-
 export default function () {
-    // var res = http.get('http://localhost:3000/products/')
-    // console.log('Response time for connection to Products was ' + String(res.timings.duration) + ' ms');
-
     // var res = http.get('http://localhost:3000/products/1')
     // console.log('Response time for Product Info-General API was ' + String(res.timings.duration) + ' ms');
 
@@ -16,3 +12,5 @@ export default function () {
 
 }
 
+
+//IN TERMINAL: k6 run --rps 1000 /Users/gretaschock/HackReactor/SDC/SDC_Overview_GS/server/scriptk6.js
